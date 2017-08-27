99adaf0304c80ddb63d77df5354ce6e203563f4e
maha <mkon.masha@gmail.com>
2017-08-27 11:59:55 +0300

    do robokassa

--- FINISH_ORDER.php
+++ FINISH_ORDER.php
@@ -198,19 +198,20 @@ echo <<<_END
 _END;
 
 
-print "<html>".
-    "<form action='https://merchant.roboxchange.com/Index.aspx' method=POST>".
-    "<input type=hidden name=MrchLogin value=$mrh_login>".
-    "<input type=hidden name=OutSum value=$out_summ>".
-    "<input type=hidden name=InvId value=$inv_id>".
-    "<input type=hidden name=Desc value='$inv_desc'>".
-    "<input type=hidden name=SignatureValue value=$crc>".
-    "<input type=hidden name=Shp_item value='$shp_item'>".
-    "<input type=hidden name=IncCurrLabel value=$in_curr>".
-    "<input type=hidden name=Culture value=$culture>".
-    "<input type=submit value='Оплата'>".
-    "</form></html>";
-
+echo <<<_END
+    <form action='https://merchant.roboxchange.com/Index.aspx' method=POST>
+    <input type=hidden name=MrchLogin value=$mrh_login>
+    <input type=hidden name=OutSum value=$out_summ>
+    <input type=hidden name=InvId value=$inv_id>
+    <input type=hidden name=Desc value='$inv_desc'>
+    <input type=hidden name=SignatureValue value=$crc>
+    <input type=hidden name=Shp_item value='$shp_item'>
+    <input type=hidden name=IncCurrLabel value=$in_curr>
+    <input type=hidden name=Culture value=$culture>
+    <input type=hidden name=IsTest value=1>
+    <input type=submit value='Оплата'>
+    </form>
+_END;
 
 echo <<<_END
       
