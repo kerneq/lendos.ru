30a9a42cd10ccb9054a5d91e96f7bd64e154822a
maha <mkon.masha@gmail.com>
2017-08-27 12:08:26 +0300

    do robokassa

--- FINISH_ORDER.php
+++ FINISH_ORDER.php
@@ -139,7 +139,7 @@ $rows = $result->num_rows-1;
 $result->data_seek($rows);
 $row = $result->fetch_array(MYSQLI_NUM);
 
-// $bd->update(NULL, $_POST['email'], NULL, NULL, NULL);
+$bd->update(NULL, $_POST['email'], NULL, NULL, NULL);
 $mrh_login = "lendosme";
 //password1
 $mrh_pass1 = "a1EH0K5FXTUfOfK5asP3";
@@ -184,7 +184,8 @@ echo <<<_END
                            id="exampleInputEmail1"
                            placeholder="$user_email"
                             style="width: 40%"
-                            name="email">
+                            name="email"
+                            required="required">
                 </div>
                 <!-- phone -->
                 <div class="form-group">
